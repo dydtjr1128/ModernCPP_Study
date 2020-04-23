e3b3ef7ad6f8333f2ecf8965702dd1dd83a9d962
Choi Yong Seok <dydtjr1994@gmail.com>
2020-04-23 09:42:53 +0900

    Add compare with std::string::append() vs std::stringstream::append()

---
+++ string vs stringstream/string vs stringstream.cpp
@@ -0,0 +1,44 @@
+﻿#include <chrono>
+#include <iostream>
+#include <sstream>
+
+int main() {
+
+	int n = 10000000;
+	std::string a = "have";
+	std::string b = "a";
+	std::string c = "good";
+	std::string d = "day";
+
+	volatile unsigned int count = 0;
+	
+	auto s = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) { // 2등
+		std::ostringstream os;
+		os << a << " " << b << " " << c << " " << d << "\r\n";
+		std::string t = os.str();
+		count += t.size();
+	}
+	std::chrono::duration<double> second = std::chrono::system_clock::now() - s;
+	std::cout << "ostringstream : " << second.count() << " second, count : " << count << std::endl;
+
+	count = 0;
+	s = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) { // 가장 빠름
+		std::string t;
+		t.append(a).append(" ").append(b).append(" ").append(c).append(" ").append(d).append("\r\n");
+		count += t.size();
+	}
+	second = std::chrono::system_clock::now() - s;
+	std::cout << "string : " << second.count() << " second, count : " << count << std::endl;
+
+
+	count = 0;
+	s = std::chrono::system_clock::now();
+	for (int i = 0; i < n; ++i) { // 3등
+		std::string t = a + " " + b + " " + c + " " + d + "\r\n";
+		count += t.size();
+	}
+	second = std::chrono::system_clock::now() - s;
+	std::cout << "string add : " << second.count() << " second, count : " << count << std::endl;
+}

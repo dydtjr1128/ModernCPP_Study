3a73b4b2e546dbcfe95045c81308c7fae6e90fe2
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-09 18:48:33 +0900

    Add Range-base-loop test

---
+++ Range-based-loop/Range-based-loop.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+//void testLoop(int array[]) {
+//    for (int num : array) {
+//
+//    }
+//}
+
+void testLoop(int (&array)[5]) {
+    for (int num : array) {
+        std::cout << "int " << num << std::endl;
+    }
+}
+
+int main() {
+    int array[] = { 1,2,3,4,5 };    ;
+
+    for (int num : array) {
+        std::cout << "int " << num << std::endl;
+    }
+    testLoop(array);
+
+}

474aac365c756c912d729c1050fa1efa9bde3ee1
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-11 19:02:39 +0900

    Add file read test code

---
+++ filesystem/filesystem.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include<fstream>
+#include <string>
+
+class FileReader {
+public:
+	void TestReceive();
+private:
+
+};
+
+void FileReader::TestReceive() {
+	//system("dir");
+	std::string resultFileaa = "./test.txt";
+	std::ifstream readStream(resultFileaa, std::ifstream::in);
+
+	char referenceFileBufferSize[400];
+	char referenceFileBuffer[1000];
+	std::string line;
+	if (readStream.is_open()) {
+		while (std::getline(readStream, line)) {
+			std::cout << line << std::endl;
+		}
+		readStream.close();
+
+	}
+
+
+	if (readStream.is_open()) {
+		while (std::getline(readStream, line)) {
+			std::cout << line << std::endl;
+		}
+		readStream.close();
+
+	}
+	else {
+		std::cout << "f" << std::endl;
+	}
+}
+
+int main()
+{
+	FileReader reader;
+	reader.TestReceive();
+}
